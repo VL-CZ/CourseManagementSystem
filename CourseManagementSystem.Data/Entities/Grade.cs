542c550ccebd164e5461b77961eb88cfcbee09ad
Vojtech Lengal <vojta.lengal@gmail.com>
2020-09-19 22:20:03 +0200

    added data annotations to entity classes

--- CourseManagementSystem.Data/Entities/Grade.cs
+++ CourseManagementSystem.Data/Entities/Grade.cs
@@ -7,10 +7,22 @@ namespace CourseManagementSystem.Data.Entities
 {
     public class Grade
     {
+        public Grade(int value, string comment, Person student)
+        {
+            Value = value;
+            Comment = comment;
+            Student = student;
+        }
+
         [Key]
         public int ID { get; set; }
+
+        [Required]
         public int Value { get; set; }
+        
         public string Comment { get; set; }
+
+        [Required]
         public Person Student { get; set; }
     }
 }
--- CourseManagementSystem.Data/Entities/Person.cs
+++ CourseManagementSystem.Data/Entities/Person.cs
@@ -7,10 +7,22 @@ namespace CourseManagementSystem.Data.Entities
 {
     public class Person
     {
+        public Person(string name, string email)
+        {
+            Name = name;
+            Email = email;
+            Grades = new List<Grade>();
+        }
+
         [Key]
         public int ID { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Email { get; set; }
+
         public ICollection<Grade> Grades { get; set; }
     }
 }
