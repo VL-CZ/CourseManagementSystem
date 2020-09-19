49d8f77a5285b9a011c46964bdb277501a3ba6c8
Vojtech Lengal <vojta.lengal@gmail.com>
2020-09-19 13:34:27 +0200

    rename project

---
+++ DAL/Models/Grade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Models
+{
+    public class Grade
+    {
+        public int ID { get; set; }
+        public int Value { get; set; }
+        public string Comment { get; set; }
+        public Person Student { get; set; }
+    }
+}
---
+++ DAL/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public ICollection<Grade> Grades { get; set; }
+    }
+}
