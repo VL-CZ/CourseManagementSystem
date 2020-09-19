8c8b09f8d2c1ff9e06ad55d225edd7c882042582
Vojtech Lengal <vojta.lengal@gmail.com>
2020-09-19 14:24:34 +0200

    EF core added to CMS.Data project

---
+++ CourseManagementSystem.Data/CMSDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseManagementSystem.Data
+{
+    class CMSDbContext : DbContext
+    {
+        private const string connectionString = "Server=.;Database=CMS;Trusted_Connection=True;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public DbSet<Person> People { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
---
+++ CourseManagementSystem.Data/Migrations/20200919121923_DB created.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourseManagementSystem.Data.Migrations
+{
+    public partial class DBcreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Grades",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<int>(nullable: false),
+                    Comment = table.Column<string>(nullable: true),
+                    StudentID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grades", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Grades_People_StudentID",
+                        column: x => x.StudentID,
+                        principalTable: "People",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Grades_StudentID",
+                table: "Grades",
+                column: "StudentID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Grades");
+
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
