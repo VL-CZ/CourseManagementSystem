3568b4b2c95650b72cc7553ac3d5b7dcf0ec8570
Vojtech Lengal <vojta.lengal@gmail.com>
2020-09-21 21:35:01 +0200

    code cleanup, added student-detail component

---
+++ CourseManagementSystem.API/ViewModels/GradeVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem.API.ViewModels
+{
+    public class GradeVM
+    {
+        public int Value { get; set; }
+        public string Topic { get; set; }
+        public string Comment { get; set; }
+    }
+}
