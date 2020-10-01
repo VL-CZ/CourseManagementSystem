860ae8384038906933c4e52adeb17cb5d10d8028
Vojtech Lengal <vojta.lengal@gmail.com>
2020-10-01 21:14:33 +0200

    bugs fixed

--- CourseManagementSystem.API/Controllers/GradesController.cs
+++ CourseManagementSystem.API/Controllers/GradesController.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CourseManagementSystem.API.Data;
-using CourseManagementSystem.API.ViewModels;
-using CourseManagementSystem.Data;
-using Microsoft.AspNetCore.Authorization;
+﻿using CourseManagementSystem.Data;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
