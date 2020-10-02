0d6ddf9fc1ea29a93d1ed553ce8813294fa4ece6
Vojtech Lengal <vojta.lengal@gmail.com>
2020-10-02 20:40:42 +0200

    comments extended

--- CourseManagementSystem.Data/Models/Person.cs
+++ CourseManagementSystem.Data/Models/Person.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace CourseManagementSystem.Data.Models
 {
+    /// <summary>
+    /// class representing user of the application
+    /// </summary>
     public class Person : IdentityUser
     {
         public ICollection<Grade> Grades { get; set; }
 
+        /// <summary>
+        /// add new grade to this person
+        /// </summary>
+        /// <param name="g"></param>
         public void AssignGrade(Grade g)
         {
             Grades.Add(g);
