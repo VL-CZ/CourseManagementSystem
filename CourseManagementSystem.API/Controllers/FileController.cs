336ca702262abc1b387f315d10e3d20b56d6e594
Vojtech Lengal <vojta.lengal@gmail.com>
2020-10-11 22:28:28 +0200

    CourseFile entity and controller added

---
+++ CourseManagementSystem.API/Controllers/FileController.cs
@@ -0,0 +1,24 @@
+﻿using CourseManagementSystem.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseManagementSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private CMSDbContext dbContext;
+
+        public FileController(CMSDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // POST api/<FileController>/upload
+        [HttpPost("upload")]
+        public void Upload([FromBody] FormFile file)
+        {
+        }
+    }
+}
---
+++ CourseManagementSystem.Data/Models/CourseFile.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CourseManagementSystem.Data.Models
+{
+    public class CourseFile
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public FormFile File { get; set; }
+    }
+}
