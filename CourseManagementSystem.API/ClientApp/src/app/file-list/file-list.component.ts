08c93edbbf9bc577de97acc70f42719256f07e71
Vojtech Lengal <vojta.lengal@gmail.com>
2020-10-18 20:13:22 +0200

    files displayed in table, added buton for removing grades

--- CourseManagementSystem.API/ClientApp/src/app/file-list/file-list.component.ts
+++ CourseManagementSystem.API/ClientApp/src/app/file-list/file-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {RoleAuthService} from '../role-auth.service';
 import {FileService} from '../file.service';
+import {FileVM} from '../viewmodels/fileVM';
 
 @Component({
   selector: 'app-file-list',
@@ -16,13 +17,17 @@ export class FileListComponent implements OnInit {
 
   public isAdmin: boolean;
   public fileToUpload: File;
-  public fileIDs: number[] = [];
+  public uploadedFiles: FileVM[] = [];
 
   constructor(roleAuthService: RoleAuthService, fileService: FileService) {
     this.fileService = fileService;
 
     roleAuthService.isAdmin().subscribe(result => {
       this.isAdmin = result.isAdmin;
+    });
+
+    fileService.getAll().subscribe(result => {
+      this.uploadedFiles = result;
     });
   }
 
@@ -35,10 +40,18 @@ export class FileListComponent implements OnInit {
   }
 
   public uploadFile(): void {
-    this.fileService.uploadFile(this.fileToUpload).subscribe(result => {
-      this.fileIDs.push(result);
+    this.fileService.upload(this.fileToUpload).subscribe(result => {
+      this.uploadedFiles.push(result);
     });
     this.fileToUpload = null;
-    this.inputFile.nativeElement.value = '';
+    this.inputFile.nativeElement.value = ''; // clear file input
+  }
+
+  public removeFile(fileId: number): void {
+    this.fileService.delete(fileId).subscribe();
+    this.uploadedFiles = this.uploadedFiles.filter(f => f.id !== fileId);
+  }
+
+  public downloadFile(fileId: number): void {
   }
 }
--- CourseManagementSystem.API/ClientApp/src/app/file.service.ts
+++ CourseManagementSystem.API/ClientApp/src/app/file.service.ts
@@ -2,6 +2,7 @@ import {Inject, Injectable} from '@angular/core';
 import {ApiService} from './api.service';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {FileVM} from './viewmodels/fileVM';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,18 @@ export class FileService extends ApiService {
     super(http, baseUrl);
   }
 
-  public uploadFile(file: File): Observable<number> {
+  public upload(file: File): Observable<FileVM> {
     const formData: FormData = new FormData();
     formData.append('file', file);
 
-    return this.http.post<number>(this.baseUrl + 'api/file/upload', formData);
+    return this.http.post<FileVM>(this.baseUrl + 'api/file/upload', formData);
+  }
+
+  public getAll(): Observable<FileVM[]> {
+    return this.http.get<FileVM[]>(this.baseUrl + 'api/file');
+  }
+
+  public delete(fileId: number): Observable<{}> {
+    return this.http.delete(this.baseUrl + 'api/file/delete/' + fileId);
   }
 }
