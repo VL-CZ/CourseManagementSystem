ea2dfd5f6ef7bc3ebc8163772faa4a96645cea67
Vojtech Lengal <vojta.lengal@gmail.com>
2020-10-19 21:38:56 +0200

    course entity added

---
+++ CourseManagementSystem.Data/Models/Course.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CourseManagementSystem.Data.Models
+{
+    public class Course
+    {
+        public Course(string name, Person admin)
+        {
+            Name = name;
+            Admin = admin;
+            Members = new List<Person>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// name of the course
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// admin of the course
+        /// </summary>
+        [Required]
+        public Person Admin { get; set; }
+
+        /// <summary>
+        /// members of the course (except admin)
+        /// </summary>
+        public ICollection<Person> Members { get; set; }
+    }
+}
--- CourseManagementSystem.Data/Models/Person.cs
+++ CourseManagementSystem.Data/Models/Person.cs
@@ -11,7 +11,15 @@ namespace CourseManagementSystem.Data.Models
     /// </summary>
     public class Person : IdentityUser
     {
+        /// <summary>
+        /// grades of this student
+        /// </summary>
         public ICollection<Grade> Grades { get; set; }
+
+        /// <summary>
+        /// courses, where the user is enrolled
+        /// </summary>
+        public ICollection<Course> Courses { get; set; }
 
         /// <summary>
         /// add new grade to this person
