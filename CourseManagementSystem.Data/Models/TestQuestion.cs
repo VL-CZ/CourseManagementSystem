dd2bd2dffb06b1a2b428fe67b8f24eb1e16aa234
Vojtech Lengal <vojta.lengal@gmail.com>
2021-01-31 22:39:20 +0100

    added classes representing tests and questions

---
+++ CourseManagementSystem.Data/Models/TestQuestion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseManagementSystem.Data.Models
+{
+    public class TestQuestion
+    {
+        public int Number { get; set; }
+
+        public string QuestionText { get; set; }
+
+        public string CorrectAnswer { get; set; }
+
+        public TestQuestion(int number, string text, string correctAnswer)
+        {
+            Number = number;
+            QuestionText = text;
+            CorrectAnswer = correctAnswer;
+        }
+    }
+}
---
+++ CourseManagementSystem.Data/Models/TestSubmission.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseManagementSystem.Data.Models
+{
+    public class TestSubmission
+    {
+        public Test Test { get; }
+
+        public CourseMember Student { get; }
+
+        public ICollection<string> Answers { get; }
+
+        public TestSubmission(Test test, ICollection<string> answers)
+        {
+            Test = test;
+            Answers = answers;
+        }
+    }
+}
