08e740dccfa35346fd0049454110d74c72a1d187
Vojtech Lengal <vojta.lengal@gmail.com>
2021-01-31 22:39:39 +0100

    unit tests added

---
+++ CourseManagementSystem.UnitTests/TestSubmissionTests.cs
@@ -0,0 +1,15 @@
+using CourseManagementSystem.Data.Models;
+using System;
+using Xunit;
+
+namespace CourseManagementSystem.UnitTests
+{
+    public class TestSubmissionTests
+    {
+        [Fact]
+        public void CheckPoints()
+        {
+            
+        }
+    }
+}
