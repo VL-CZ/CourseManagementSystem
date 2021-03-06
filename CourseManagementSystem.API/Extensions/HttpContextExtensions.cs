56c9e89a3a0bea3c62b365ccb534a26827ea46f7
Vojtech Lengal <vojta.lengal@gmail.com>
2021-03-06 12:30:11 +0100

    added extension method for getting current user id

---
+++ CourseManagementSystem.API/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem.API.Extensions
+{
+    /// <summary>
+    /// class containg extension methods for <see cref="HttpContext"/>
+    /// </summary>
+    public static class HttpContextExtensions
+    {
+        /// <summary>
+        /// get ID of the current user
+        /// </summary>
+        /// <param name="httpContext">given HttpContext</param>
+        /// <returns>ID of the current user</returns>
+        public static string GetCurrentUserId(this HttpContext httpContext)
+        {
+            return httpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+        }
+    }
+}
