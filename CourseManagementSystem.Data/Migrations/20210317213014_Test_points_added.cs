d48f101285ca9c01b8cdea58940a87bc13f963aa
Vojtech Lengal <vojta.lengal@gmail.com>
2021-03-17 22:39:46 +0100

    migrations added

---
+++ CourseManagementSystem.Data/Migrations/20210317213014_Test_points_added.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourseManagementSystem.Data.Migrations
+{
+    public partial class Test_points_added : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Points",
+                table: "TestSubmissionAnswers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Points",
+                table: "TestQuestions",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Points",
+                table: "TestSubmissionAnswers");
+
+            migrationBuilder.DropColumn(
+                name: "Points",
+                table: "TestQuestions");
+        }
+    }
+}
