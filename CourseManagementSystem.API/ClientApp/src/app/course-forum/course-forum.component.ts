0a0e6b1eb5257718a449553e2807a9c4e2608a2f
Vojtech Lengal <vojta.lengal@gmail.com>
2021-04-03 12:30:00 +0200

    unused param removed

--- CourseManagementSystem.API/ClientApp/src/app/course-forum/course-forum.component.ts
+++ CourseManagementSystem.API/ClientApp/src/app/course-forum/course-forum.component.ts
@@ -68,7 +68,7 @@ export class CourseForumComponent implements OnInit {
    * add a new post
    */
   public addPost(): void {
-    this.forumPostService.add(this.postToAdd, this.courseId).subscribe(post => {
+    this.forumPostService.add(this.postToAdd, this.courseId).subscribe(() => {
       this.reloadPosts();
       this.postToAdd = new ForumPostVM();
     });
