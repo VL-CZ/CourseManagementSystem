0601bb4301659593f4178e2db18dfea0fb7278d0
Vojtech Lengal <vojta.lengal@gmail.com>
2021-04-05 12:21:39 +0200

    people service updated

--- CourseManagementSystem.Services/Implementations/PeopleService.cs
+++ CourseManagementSystem.Services/Implementations/PeopleService.cs
@@ -1,6 +1,9 @@
 ﻿using CourseManagementSystem.Data;
 using CourseManagementSystem.Data.Models;
 using CourseManagementSystem.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CourseManagementSystem.Services.Implementations
 {
@@ -14,9 +17,39 @@ namespace CourseManagementSystem.Services.Implementations
         }
 
         /// <inheritdoc/>
+        public void EnrollTo(Person person, Course course)
+        {
+            var cm = new CourseMember(person, course);
+            dbContext.CourseMembers.Add(cm);
+            dbContext.SaveChanges();
+        }
+
+        /// <inheritdoc/>
         public Person GetById(string personId)
         {
             return dbContext.Users.Find(personId);
+        }
+
+        /// <inheritdoc/>
+        public CourseMember GetCourseMembership(Person person, Course course)
+        {
+            return dbContext.CourseMembers.Include(cm => cm.Course).Include(cm => cm.User)
+                .Where(cm => cm.User.Id == person.Id)
+                .Where(cm => cm.Course.Id == course.Id)
+                .SingleOrDefault();
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<Course> GetManagedCourses(string personId)
+        {
+            return dbContext.Courses.Include(c => c.Admin).Where(c => c.Admin.Id == personId);
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<Course> GetMemberCourses(string personId)
+        {
+            return dbContext.CourseMembers.Include(cm => cm.Course).Include(cm => cm.User)
+                .Where(cm => cm.User.Id == personId).Select(cm => cm.Course);
         }
     }
 }
--- CourseManagementSystem.Services/Interfaces/IPeopleService.cs
+++ CourseManagementSystem.Services/Interfaces/IPeopleService.cs
@@ -1,4 +1,5 @@
 ﻿using CourseManagementSystem.Data.Models;
+using System.Collections.Generic;
 
 namespace CourseManagementSystem.Services.Interfaces
 {
@@ -10,5 +11,36 @@ namespace CourseManagementSystem.Services.Interfaces
         /// <param name="personId">identifier of the person</param>
         /// <returns></returns>
         Person GetById(string personId);
+
+        /// <summary>
+        /// enroll person to a course
+        /// </summary>
+        /// <param name="person">person that we enroll to a course</param>
+        /// <param name="course">course where to enroll</param>
+        void EnrollTo(Person person, Course course);
+
+        /// <summary>
+        /// get managed courses of the person (e.g. all courses where the person is an admin)
+        /// </summary>
+        /// <param name="personId">id of the person</param>
+        /// <returns></returns>
+        IEnumerable<Course> GetManagedCourses(string personId);
+
+        /// <summary>
+        /// get all courses whose member the given person is
+        /// </summary>
+        /// <param name="personId">id of the person</param>
+        /// <returns></returns>
+        IEnumerable<Course> GetMemberCourses(string personId);
+
+        /// <summary>
+        /// get course membership of the <paramref name="person"/> in <paramref name="course"/>
+        /// <br/>
+        /// or return null if the <paramref name="person"/> is not member of <paramref name="course"/>
+        /// </summary>
+        /// <param name="person">selected person</param>
+        /// <param name="course">selected course</param>
+        /// <returns></returns>
+        CourseMember GetCourseMembership(Person person, Course course);
     }
 }
