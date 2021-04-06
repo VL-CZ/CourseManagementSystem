408ddecc2d6b7f5e183dbab4e3ca28450a92c4fd
Vojtech Lengal <vojta.lengal@gmail.com>
2021-04-06 23:15:43 +0200

    added TestSubmission status and submitted datetime to model

---
+++ CourseManagementSystem.Data/Migrations/20210406211306_TestSubmissionInfo_added.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourseManagementSystem.Data.Migrations
+{
+    public partial class TestSubmissionInfo_added : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsReviewed",
+                table: "TestSubmissions",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SubmittedDateTime",
+                table: "TestSubmissions",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsReviewed",
+                table: "TestSubmissions");
+
+            migrationBuilder.DropColumn(
+                name: "SubmittedDateTime",
+                table: "TestSubmissions");
+        }
+    }
+}
