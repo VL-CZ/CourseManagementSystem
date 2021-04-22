f220cb60008bb3cc4bc2c1fcd89cac09beb9cf4c
Vojtech Lengal <vojta.lengal@gmail.com>
2021-04-22 11:23:45 +0200

    custom validation attributes added

---
+++ CourseManagementSystem.API/Validation/Attributes/PositiveIntRangeAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseManagementSystem.API.Validation.Attributes
+{
+    /// <summary>
+    /// Validation attribute that validates if the integer value is >= 1
+    /// <br/>
+    /// if validation fails <see cref="defaultErrorMessage"/> is displayed
+    /// </summary>
+    public class PositiveIntRangeAttribute : RangeAttribute
+    {
+        /// <summary>
+        /// default error message displayed ({0} is replaced by the field name that this attribute belongs to)
+        /// </summary>
+        public const string defaultErrorMessage = "The field {0} must be a positive integer (greater than or equal to 1)";
+
+        public PositiveIntRangeAttribute() : base(1, int.MaxValue)
+        {
+            ErrorMessage = defaultErrorMessage;
+        }
+    }
+}
---
+++ CourseManagementSystem.API/Validation/Attributes/RequiredWithDefaultErrorMessageAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseManagementSystem.API.Validation.Attributes
+{
+    /// <summary>
+    /// Validation attribute that marks this value as required
+    /// <br/>
+    /// if validation fails <see cref="defaultErrorMessage"/> is displayed
+    /// </summary>
+    public class RequiredWithDefaultErrorMessageAttribute : RequiredAttribute
+    {
+        /// <summary>
+        /// default error message displayed ({0} is replaced by the field name that this attribute belongs to)
+        /// </summary>
+        public const string defaultErrorMessage = "The field {0} is required";
+
+        public RequiredWithDefaultErrorMessageAttribute()
+        {
+            ErrorMessage = defaultErrorMessage;
+        }
+    }
+}
