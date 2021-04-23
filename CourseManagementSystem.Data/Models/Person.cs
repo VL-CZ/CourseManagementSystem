ed0815976add9b631788ef5f1a18b497e475ba6f
Vojtech Lengal <vojta.lengal@gmail.com>
2021-04-23 10:38:26 +0200

    added course archive field

--- CourseManagementSystem.Data/Models/Person.cs
+++ CourseManagementSystem.Data/Models/Person.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CourseManagementSystem.Data.Models
 {
@@ -15,6 +12,5 @@ namespace CourseManagementSystem.Data.Models
         /// grades of this student
         /// </summary>
         public ICollection<CourseMember> CourseMemberships { get; set; } = new List<CourseMember>();
-
     }
 }
