3ba349313cb0ec91c26189442637fd39f17aacd5
Vojtech Lengal <vojta.lengal@gmail.com>
2021-05-21 20:16:50 +0200

    dateTime formatter added

---
+++ CourseManagementSystem.API/ClientApp/src/app/utils/dateTimeFormatter.ts
@@ -0,0 +1,12 @@
+/**
+ * class for formatting date-time according to local settings
+ */
+export class DateTimeFormatter {
+  /**
+   * format the date-time string into local date format
+   * @param dateTimeString date in string
+   */
+  public formatToLocal(dateTimeString: string): string {
+    return dateTimeString;
+  }
+}
