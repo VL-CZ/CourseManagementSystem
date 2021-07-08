31f9b52d3bbe198e76b0c6ae0a2e0eba39a0cd26
Vojtech Lengal <vojta.lengal@gmail.com>
2021-07-08 11:13:32 +0200

    fixed bug with self-removing admin

--- CourseManagementSystem.Services/Interfaces/ICourseAdminService.cs
+++ CourseManagementSystem.Services/Interfaces/ICourseAdminService.cs
@@ -9,5 +9,12 @@ namespace CourseManagementSystem.Services.Interfaces
         /// </summary>
         /// <param name="courseAdminId">identifier of the <see cref="CourseAdmin"/> to remove</param>
         void RemoveById(string courseAdminId);
+
+        /// <summary>
+        /// get identifier of <see cref="Person"/> that this course admin object belongs to
+        /// </summary>
+        /// <param name="courseAdminId">identifier of the course admin object</param>
+        /// <returns></returns>
+        string GetPersonId(string courseAdminId);
     }
 }
